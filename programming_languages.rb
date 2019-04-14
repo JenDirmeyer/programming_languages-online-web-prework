77957f683babfd749a961169b35fb7f1c430f89b
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-14 15:20:56 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -11,7 +11,8 @@ def reformat_languages(languages)
       value.each do |language_names, info|
         if value.include?(language)
             keys << key
-            new_hash[language_names][:style] = keys  
+            new_hash[language_names][:style] = keys
+            keys= []
             binding.pry  
         end 
       end
